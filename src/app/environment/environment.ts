bd7d771d496888d922411d09c3b3804fbccab76b
Lelikbjmak <denissolovey990@gmail.com>
2023-05-01 00:55:52 +0300

    feature: Implement environment file with API routes.

---
+++ src/app/environment/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    countryRoute: "http://localhost:8080/api/v1/covid/countries",
+    currentDayCovidCasesRoute: "http://localhost:8080/api/v1/covid/countries/summary",
+    anyDayCovidCasesRoute: "http://localhost:8080/api/v1/covid/countries/day/",
+    termCovidCasesRoute: "http://localhost:8080/api/v1/covid/countries/term"
+}
